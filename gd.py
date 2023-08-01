26a002b8cbe8dfe952e830a096793c2740f3317c
Damontools32 <123245734+Damontools32@users.noreply.github.com>
2023-08-01 16:18:36 +0330

    Update gd.py

--- gd.py
+++ gd.py
@@ -1,30 +1,39 @@
 import telebot
-from pydrive.drive import GoogleDrive
+from telebot import types
 
-TELEGRAM_TOKEN = 'xxxxxxxxx:yyyyyyyyy'
+from google.oauth2 import service_account
+from googleapiclient.http import MediaFileUpload
+from googleapiclient.discovery import build
 
-bot = telebot.TeleBot(TELEGRAM_TOKEN) 
+bot = telebot.TeleBot("TOKEN")
+
+SERVICE_ACCOUNT_FILE = 'google-credentials.json'
+
+drive_service = build('drive', 'v3', credentials=service_account.Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE))
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+  bot.reply_to(message, "Hi, send me a file to upload")
 
 @bot.message_handler(content_types=['document'])
-def handle_document(message):
+def handle_doc(message):
+  try:
+    file_id = message.document.file_id
+    file = bot.get_file(file_id)
+    downloaded_file = bot.download_file(file.file_path)
 
-  file_id = message.document.file_id
-  
-  file = bot.get_file(file_id)
-  
-  downloaded_file = bot.download_file(file.file_path)
+    drive_file = drive_service.files().create(
+      media_body=MediaFileUpload(downloaded_file),
+      body={
+        'name': file.file_name
+      }
+    )
 
-  gauth = GoogleAuth()
-  
-  # Authorization 
-  gauth.CommandLineAuth() 
+    drive_file.execute()
 
-  drive = GoogleDrive(gauth)
+    bot.reply_to(message, "File uploaded successfully!")
 
-  uploaded_file = drive.CreateFile({'title': file.file_name})
-  uploaded_file.SetContentString(downloaded_file)
-  uploaded_file.Upload()
-
-  bot.reply_to(message, "File uploaded successfully!")
+  except Exception as e:
+    bot.reply_to(message, f"Oops, something went wrong: {e}")
 
 bot.polling()
