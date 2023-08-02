6ad0c1a242b4d03cfae3c2d67064e1346ad763d9
Damontools32 <123245734+Damontools32@users.noreply.github.com>
2023-08-03 01:55:34 +0330

    Update gd.py

--- gd.py
+++ gd.py
@@ -3,21 +3,22 @@ import re
 
 bot = telebot.TeleBot("TOKEN")
 
-@bot.message_handler(func=lambda msg: msg.text.startswith("https://drive.google.com"))
-def get_filename(message):
-  bot.reply_to(message, "لطفا نام فایل را بفرستید:")
+@bot.message_handler(func=lambda message: message.text.startswith("https://drive.google.com"))
+def handle_google_drive_link(message):
 
-  @bot.message_handler(func=lambda msg: True) 
+  file_id = re.search(r"(?:/d/|id=)(\w+)", message.text).group(1)
+
+  bot.send_message(message.chat.id, "لطفا نام فایل را ارسال کنید:")
+
+  @bot.message_handler(func=lambda message: True)
   def send_download_link(message):
     filename = message.text
-    file_id = re.search(r"/d/(.+)", message.text).group(1)
-
-    download_link = f"https://drive.google.com/uc?id={file_id}" 
+    download_link = f"https://drive.google.com/uc?id={file_id}"
     
     bot.send_message(
-      message.chat.id,
-      f"لینک دانلود <a href='{download_link}'>{filename}</a>",
-      parse_mode='HTML'
+      chat_id=message.chat.id,
+      text=f"لینک دانلود فایل <b>{filename}</b>:\n{download_link}",
+      parse_mode='html'
     )
 
 bot.polling()
